cb29428c2c3760957b5a65a5ac0acb30b3e82ee2
sphynkx <sfynkx@gmail.com>
2026-05-26 08:42:31 +0300

    Rework dropdown menu with updated framework. Remove old realization - topmenu.

--- module/ic/topbar.m
+++ module/ic/topbar.m
@@ -5,6 +5,11 @@ IcTopBar: module
 {
 	PATH: con "/dis/ic/topbar.dis";
 
+	CmdNone: con 0;
+	CmdHandled: con 1;
+
+	CmdOptionsScreensavers: con 1001;
+
 	init: fn();
 	newbar: fn(): ref IcState->TopBarState;
 	build: fn(state: ref IcState->AppState, bar: ref IcState->TopBarState, rect: IcLayout->Rect): int;
@@ -13,4 +18,5 @@ IcTopBar: module
 	toggle: fn(bar: ref IcState->TopBarState);
 	close: fn(bar: ref IcState->TopBarState);
 	handlekey: fn(state: ref IcState->AppState, bar: ref IcState->TopBarState, k: int): int;
+	handletick: fn(state: ref IcState->AppState, bar: ref IcState->TopBarState): int;
 };
